1549853b6ef9fc66a6a4618a6198b6b270a99a5e
daniel <daniel.aquije@upch.pe>
2022-11-08 11:06:52 -0500

    version2

--- app/src/main/java/com/example/intento45/MainActivity.java
+++ app/src/main/java/com/example/intento45/MainActivity.java
@@ -1,7 +1,14 @@
+
 package com.example.intento45;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+public void irIniciar(View view){
+        Intent i = new Intent(this, Menu.class);
+        startActivity(i);
+}
+
+
 }
